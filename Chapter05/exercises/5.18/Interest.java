2f699a024074f3de25ea596ea6c35fc2679ac35f
Evangelos Panagiotis Dionysopoulos <60899527+EvanDion@users.noreply.github.com>
2020-06-19 18:04:17 +0300

    Update Interest.java
    The solution to the problem.

--- Chapter05/exercises/5.18/Interest.java
+++ Chapter05/exercises/5.18/Interest.java
@@ -6,8 +6,8 @@
  *        Created:  06/10/15 14:34:43
  *       Revision:  none
  *
- *        @Author:  Siidney Watson - siidney.watson.work@gmail.com
- *       @Version:  1.0
+ *        @Author:  Siidney Watson - siidney.watson.work@gmail.com // @Edited by : EvanDion
+ *       @Version:  1.1
  *
  * =====================================================================================
  */
@@ -16,17 +16,27 @@
 //                    figuring it out
 
 public class Interest{
-    public static void main(String[] args){
-        int amount; // amount on deposit at end of each year
-        int principal = 1000; // initial amount before interest
-        int rate = 5; // interest rate
-
-        System.out.printf("%s%20s\n", "YEAR", "Amount on deposit");
-
-        for(int year=0; year<=10; year++){
-            amount = principal * (int)Math.pow(1 + rate, year);
-
-            System.out.printf("%4d%,20d\n", year, amount);
+    public static void main(String args[]){
+        int amount; 
+        int principal = 100000; 
+        int rate = 5;
+       
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+        
+        for (int year = 1; year <= 10; year++) 
+        {
+            String beautify = "";
+            amount = (int)((principal * Math.pow(100 + rate, year)) / Math.pow(100, year));
+            
+            int dollars = amount / 100;     //  All these lines are not needed.
+            int cents = amount % 100;       //  They are here to make the final
+            if((cents / 10) < 1){           //  output a bit more appealing. 
+                if(cents != 0)              //  You can just ignore them completely
+                    cents = cents + 10;     //  and redo the final print as follows:
+                else                        //  System.out.printf("%4d%17d.%d%n", 
+                    beautify = "0";         //  year, amount / 100, amount % 100);
+            }                               //  You will save 9 lines of code this way. 
+            System.out.printf("%4d%17d.%d%s%n", year, dollars, cents, beautify);
         }
     }
 }
